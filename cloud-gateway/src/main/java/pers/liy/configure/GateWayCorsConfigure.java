fbd717dd56ffccb62dd6f1e6e5dfad6050f97e8f
Long2273864459 <Long2273864459@163.com>
2020-10-27 14:03:03 +0800

    feature: 跨域处理

---
+++ cloud-gateway/src/main/java/pers/liy/configure/GateWayCorsConfigure.java
@@ -0,0 +1,36 @@
+package pers.liy.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Author Prock.Liy
+ * @Date 2020/10/10 16:42
+ * @Description  跨域处理
+ **/
+@Configuration
+public class GateWayCorsConfigure {
+
+    /**
+     * setAllowCredentials(true)表示允许cookie跨域；
+     * addAllowedHeader(CorsConfiguration.ALL)表示请求头部允许携带任何内容；
+     * addAllowedOrigin(CorsConfiguration.ALL)表示允许任何来源；
+     * addAllowedMethod(CorsConfiguration.ALL)表示允许任何HTTP方法
+     * @return CorsFilter
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+        corsConfiguration.addAllowedOrigin(CorsConfiguration.ALL);
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+
+}
